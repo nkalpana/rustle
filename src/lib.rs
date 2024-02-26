d8971c339c1d4cdf039be60a84c8f288e4f70f1c
per-shriram <1299342+per-shriram@users.noreply.github.com>
2024-02-25 18:57:04 -0800

    first commit
    included dependency on top_english_words and called first function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+use top_english_words::get_words_range;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
+    let _first_5_words = get_words_range::<String>(..5).unwrap();
     a + b
 }
